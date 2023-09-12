aaf5befe178790076e0fbc40ce2ad75ae98787f4
A7Coder <a7coder@gmail.com>
2023-09-12 11:01:48 +0530

    Added Backend For LocalHOst

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
     env: {
       /** IT SHOUD be of WIndows Ip address Currently it is for testing only */
-        DOMAIN_URL: 'http://192.168.105.134:8070',
+        DOMAIN_URL: 'http://localhost:8000',
   
       }
 }
